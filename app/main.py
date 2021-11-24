4f930d7c90d50287f6a7f1c70c7e86da738d5f15
Deepak <deepak.rajendran123@gmail.com>
2021-11-24 13:52:59 +0530

    removed sqlalchemy code which is now handeled by alembic

--- app/main.py
+++ app/main.py
@@ -4,7 +4,8 @@ from . import models
 from .database import engine
 from .routers import posts,users,auth,vote
 
-models.Base.metadata.create_all(bind=engine)
+# Removed after creation of alembic
+# models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
